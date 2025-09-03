c3841a5202f16a9f3ad45704cec1afcb338fc527
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2025-09-03 13:37:52 +0545

    fix database config: fallback to sqlite if DATABASE_URL is missing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -82,13 +82,26 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 AUTH_USER_MODEL = "keyflow.User"
 
 # Database
-DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=False,
-    )
-}
+import dj_database_url
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
